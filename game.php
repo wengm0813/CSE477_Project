f98a61cbd23c8c048f22c8f92b06b7e3cae8f2a3
Min Weng <wengm0813@gmail.com>
2016-02-12 14:53:55 -0500

    udpated

--- game.php
+++ game.php
@@ -16,6 +16,38 @@
     <link href="style.css" type="text/css" rel="stylesheet" />
 </head>
 <body>
+<div id="centerwrap">
+<header>
+    <img src="images/title.png">
+</header>
+<form>
+    <div class="game">
 
+        <div class="row">
+            <div class="cell"></div><div class="cell"></div><div class="cell"></div>
+        </div>
+
+        <div class="row">
+            <div class="cell"></div><div class="cell"></div><div class="cell"></div>
+        </div>
+    </div>
+    <p>
+        <label>
+            <input type="radio" name="pipe" value="small" />
+            <img src="images/straight-h.png">
+        </label>
+        <label>
+            <input type="radio" name="pipe" value="small" />
+            <img src="images/ninety-es.png">
+        </label>
+     </p>
+    <p>
+    <input type="submit" name="leak" value="Rotate">
+    <input type="submit" name="leak" value="Discard">
+    <input type="submit" name="leak" value="Open Valve">
+    <input type="submit" name="leak" value="Give Up">
+    </p>
+</form>
+</div>
 </body>
 </html>

83db1d29aa9423050e9bc9917313ef1905bda787
Min Weng <wengm0813@gmail.com>
2016-02-12 14:04:02 -0500

    initialize

---
+++ game.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Min
+ * Date: 2/12/2016
+ * Time: 1:52 PM
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Project1 CheckPoint</title>
+    <link href="style.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+
+</body>
+</html>
---
+++ lib/Model.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Min
+ * Date: 2/12/2016
+ * Time: 2:01 PM
+ */
+class Model
+{
+
+}

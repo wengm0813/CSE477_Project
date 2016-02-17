3d0f5fced3d5fd630049a67ef82b266f09b88dd2
Min Weng <wengm0813@gmail.com>
2016-02-17 16:28:08 -0500

    added composer stuff

---
+++ tests/tests/SteampunkedTest.php
@@ -0,0 +1,22 @@
+<?php
+require __DIR__ . "/../../vendor/autoload.php";
+/** @file
+ * @brief Empty unit testing template
+ * @cond 
+ * @brief Unit tests for the class 
+ */
+
+use Steampunked\Steampunked as Steampunked;
+
+
+class SteampunkedTest extends \PHPUnit_Framework_TestCase
+{
+
+    const SEED = 1234;
+
+	public function test1() {
+		//$this->assertEquals($expected, $actual);
+	}
+}
+
+?>

59ae9b68b391806f0a4b274a47a5c29feb0a03b3
Min Weng <wengm0813@gmail.com>
2016-02-17 18:05:22 -0500

    modeal class

--- tests/tests/SteampunkedTest.php
+++ tests/tests/SteampunkedTest.php
@@ -1,22 +1,46 @@
 <?php
+
 require __DIR__ . "/../../vendor/autoload.php";
-/** @file
- * @brief Empty unit testing template
- * @cond 
- * @brief Unit tests for the class 
- */
 
 use Steampunked\Steampunked as Steampunked;
 
 
 class SteampunkedTest extends \PHPUnit_Framework_TestCase
 {
+	const SEED = 1234;
+	public function testConstuctor() {
 
-    const SEED = 1234;
+		$steampunked = new Steampunked(self::SEED);
+		$this->assertInstanceOf('Steampunked\Steampunked', $steampunked);
 
-	public function test1() {
-		//$this->assertEquals($expected, $actual);
+		$steampunked2 = new Steampunked();
+		$this->assertInstanceOf('Steampunked\Steampunked', $steampunked2);
 	}
+
+
+	public function testGameSize() {
+		$steampunked = new Steampunked(self::SEED);
+		$steampunked->createGame(6);
+		$this->assertEquals(6, $steampunked->getGameSize());
+
+		$steampunked2 = new Steampunked(self::SEED);
+		$this->assertEquals(false, $steampunked2->createGame(15));
+
+	}
+
+	public function testPlayerTurn() {
+		$steampunked = new Steampunked(self::SEED);
+		$this->assertEquals(1, $steampunked->getTurn());
+		$this->assertEquals(2, $steampunked->switchTurn());
+		$this->assertEquals(2, $steampunked->getTurn());
+		$this->assertEquals(1, $steampunked->switchTurn());
+		$this->assertEquals(1, $steampunked->getTurn());
+
+
+
+	}
+
 }
 
+/// @endcond
 ?>
